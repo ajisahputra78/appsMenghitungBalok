81aa5e6600bd7b08bf2ce163d657cb1b4b0677ff
Aji Sahputra <aji.saputro@studens.amikom.ac.id>
2020-09-11 21:36:09 +0700

    Initial Commit

---
+++ appsMenghitungBalok/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  appsMenghitungBalok
+//
+//  Created by Aji Sahputra78 on 11/09/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
